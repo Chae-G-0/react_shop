e83eb480520cd3759e6f846245f41c1ac06185d5
Chae-G-0 <corkdud@naver.com>
2023-01-10 23:59:21 +0900

    230110

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import "./App.css";
+import products from "./data";
 
 function App() {
+
+  const [shoes] = useState()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">SHOP</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="mainBg"></div>
+      <div className="container">
+        <div className="row">
+          {
+            products.map(it => {
+              return (
+                <div>
+                  {it.title}
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
   );
 }
